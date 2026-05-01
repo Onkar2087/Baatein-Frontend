d79b84e83add4bc251d239e0e761697862f981d4
Onkar2087 <114167896+Onkar2087@users.noreply.github.com>
2026-05-01 06:04:29 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { store } from './redux/store.js'
-export const serverurl = "http://localhost:8000"
+export const serverurl = import.meta.env.VITE_BACKEND_URL
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter> {/* react-router */}
